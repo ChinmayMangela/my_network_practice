b0f4fa1c96024fde0ac26a6bba5c93b722bc9cc7
apple <mangelachinmay7@gmail.com>
2025-01-14 21:13:49 +0530

    Implement Recipe model class with 2 enum's (Difficulty and MealType)
     The `Recipe` class contains all the defined members: `name`, `ingredients`, `instructions`, `prepTimeMinutes`, `cookTimeMinutes`, `servings`, `imageUrl`, `difficulty`, `caloriesPerServing`, `tags`, `image`, `rating`, `reviewCount`, `mealType`.

--- lib/recipes/data/models/Recipe.dart
+++ lib/recipes/data/models/Recipe.dart
@@ -1,3 +1,5 @@
+
+
 enum Difficulty {
   easy,
   medium,
@@ -48,22 +50,4 @@ class Recipe {
     required this.mealType,
   });
 
-  factory Recipe.fromJson(Map<String, dynamic> recipeData) {
-    return Recipe(
-      name: recipeData['name'],
-      ingredients: recipeData['ingredients'] as List<String>?,
-      instructions: recipeData['instructions'] as List<String>?,
-      prepTimeMinutes: recipeData['prepTimeMinutes'],
-      cookTimeMinutes: recipeData['cookTimeMinutes'],
-      servings: recipeData['servings'],
-      imageUrl: recipeData['imageUrl'],
-      difficulty: recipeData['difficulty'],
-      caloriesPerServing: recipeData['caloriesPerServing'],
-      tags: recipeData['tags'] as List<String>?,
-      image: recipeData['image'],
-      rating: recipeData['rating'],
-      reviewCount: recipeData['reviewCount'],
-      mealType: recipeData['mealType'] as List<MealType>?,
-    );
-  }
 }
